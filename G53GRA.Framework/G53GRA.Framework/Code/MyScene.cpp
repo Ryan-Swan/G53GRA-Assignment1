044ddaf8dc6a7573d75dd87d365e99f37347fec6
Ryan Swan <psyrds@nottingham.ac.uk>
2019-02-10 18:42:21 +0000

    Completed Task 1 and inclusion of G53GRA Framework
    Added scene to show tetrahedron, cube (constructed via triangles) and tetrahedron fitting inside said wireframe cube. Now utilizes G53GRA Framework instead of standalone.

---
+++ G53GRA.Framework/G53GRA.Framework/Code/MyScene.cpp
@@ -0,0 +1,23 @@
+#include "MyScene.h"
+#include "Shape.h"
+
+MyScene::MyScene(int argc, char** argv, const char *title, const int& windowWidth, const int& windowHeight)
+	: Scene(argc, argv, title, windowWidth, windowHeight)
+{
+
+}
+
+void MyScene::Initialise()
+{
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+	Shape *c = new Shape();
+	AddObjectToScene(c);
+
+}
+
+void MyScene::Projection()
+{
+	GLdouble aspect = static_cast<GLdouble>(windowWidth) / static_cast<GLdouble>(windowHeight);
+	gluPerspective(60.0, aspect, 1.0, 1000.0);
+}
